8a338da12dde2e67944493a5f333f62a76c51db1
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2025-04-15 20:48:34 -0300

    Atualizar o WelcomeActivity.kt

--- app/src/main/java/br/com/ibm/superid/WelcomeActivity.kt
+++ app/src/main/java/br/com/ibm/superid/WelcomeActivity.kt
@@ -10,17 +10,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*package br.com.ibm.superid
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
