3ad2a11e3c69cdd00767d5853732166fe2c112d5
sophiagodoy <sophia.fg@puccampinas.edu.br>
2025-04-25 10:35:51 -0300

    criando Activity para criar senha e alterar

---
+++ app/src/main/java/br/com/ibm/superid/EditPasswordActivity.kt
@@ -0,0 +1,48 @@
+// TELA PARA ALTERAR / REMOLVER SENHA
+package br.com.ibm.superid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.ibm.superid.ui.theme.SuperIDTheme
+
+class EditPasswordActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperIDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting4(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting4(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview4() {
+    SuperIDTheme {
+        Greeting4("Android")
+    }
+}
