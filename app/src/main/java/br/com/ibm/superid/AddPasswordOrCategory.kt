f4c47fd1869d442dc1e654596e06e64950a92e88
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2025-04-28 08:23:42 -0300

    Update AddPasswordOrCategory.kt
    Adição do intent para fazer a mudança de tela para cadastro de senha e cadastro de categoria

--- app/src/main/java/br/com/ibm/superid/AddPasswordOrCategory.kt
+++ app/src/main/java/br/com/ibm/superid/AddPasswordOrCategory.kt
@@ -1,5 +1,6 @@
 package br.com.ibm.superid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,12 +14,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
 
 class AddPasswordOrCategory : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +37,9 @@ class AddPasswordOrCategory : ComponentActivity() {
 
 @Composable
 fun AddPasswordOrCat(){
+    val context = LocalContext.current
+    var categoria by remember { mutableStateOf(false) }
+    var senha by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -40,10 +48,22 @@ fun AddPasswordOrCat(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = {}) {
+        Button(onClick = {
+            if(categoria){
+                val intent = Intent(context, AddCategory::class.java) //
+                context.startActivity(intent)
+            }
+        },
+            enabled = categoria
+        ) {
             Text(text = "Adicionar Categoria: ")
         }
-        Button(onClick = {}) {
+        Button(onClick = {
+            val intent = Intent(context, AddPasswordActivity::class.java) //
+            context.startActivity(intent)
+        },
+            enabled = senha
+        ) {
             Text(text = "Adicionar senha: ")
         }
     }
