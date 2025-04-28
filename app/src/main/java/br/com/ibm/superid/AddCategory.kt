9e5b6c53629455ffeac6af5b97af29913af07000
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2025-04-28 08:15:17 -0300

    Update AddCategory.kt

--- app/src/main/java/br/com/ibm/superid/AddCategory.kt
+++ app/src/main/java/br/com/ibm/superid/AddCategory.kt
@@ -4,44 +4,73 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CheckboxDefaults.colors
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.ibm.superid.ui.theme.SuperIDTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class AddCategory : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SuperIDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting3(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            PreviewAddCat()
         }
     }
 }
 
 @Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AddCat() {
+    val categoryName = remember { mutableStateOf("") }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.LightGray)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Adicionar Categoria".uppercase(),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.padding(10.dp),
+            value = categoryName.value,
+            onValueChange = { categoryName.value = it },
+            label = { Text(text = "Nome da Categoria") }
+        )
+
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF9DA783) // Esse é um tom aproximado do verde da sua imagem
+                )
+            ) {
+                Text(text = "Salvar")
+            }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview3() {
-    SuperIDTheme {
-        Greeting3("Android")
-    }
+fun PreviewAddCat() {
+    AddCat()
 }
