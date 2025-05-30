d51949ee6d7d14a273ea20ca916e96260a131bea
sophiagodoy <sophiagodoy.profissional@gmail.com>
2025-05-30 13:03:05 -0300

    função sign in atualizada

--- app/src/main/java/br/com/ibm/superid/SignInActivity.kt
+++ app/src/main/java/br/com/ibm/superid/SignInActivity.kt
@@ -55,6 +55,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import br.com.ibm.superid.ui.theme.SuperIDTheme
 import br.com.ibm.superid.ui.theme.core.util.CustomOutlinedTextField
 import br.com.ibm.superid.ui.theme.core.util.SuperIDHeader
+import br.com.ibm.superid.ui.theme.core.util.signInWithFirebaseAuth
 import kotlin.jvm.java
 
 // Declarando a Activity (signInActivity)
@@ -68,31 +69,6 @@ class SignInActivity : ComponentActivity() {
             }
         }
     }
-}
-
-// Validando as credenciais do usuário
-fun signInWithFirebaseAuth(email: String, password: String, context: Context) {
-    // Obtemos a instância do Firestore Auth
-    val auth = Firebase.auth
-
-    auth.signInWithEmailAndPassword(email, password)
-        .addOnCompleteListener { task ->
-            // Verifica se o login foi bem-sucedido
-            if (task.isSuccessful) {
-                val user = task.result?.user
-                Log.i("AUTH", "Login realizado com sucesso. UID: ${user?.uid}")
-
-                // Redireciona a tela direto para MainActivity
-                val intent = Intent(context, MainActivity::class.java)
-                context.startActivity(intent)
-            } else {
-                // Se falhou será mostrado no LogCat
-                Log.i("AUTH", "Falha ao fazer login.", task.exception)
-                // Exibe uma mensagem Toast
-                // Baseado na documentação oficial do Android: https://developer.android.com/guide/topics/ui/notifiers/toasts?hl=pt-br
-                Toast.makeText(context, "Email ou senha incorreta!", Toast.LENGTH_LONG).show()
-            }
-        }
 }
 
 // Função Composable que apresenta o formulário de login do usuário
--- app/src/main/java/br/com/ibm/superid/ui/theme/core/util/FirebaseUtilities.kt
+++ app/src/main/java/br/com/ibm/superid/ui/theme/core/util/FirebaseUtilities.kt
@@ -1,8 +1,10 @@
 package br.com.ibm.superid.ui.theme.core.util
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.widget.Toast
+import br.com.ibm.superid.MainActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.auth
@@ -26,5 +28,34 @@ fun reauthenticateUser(password: String, context: Context, onSuccess: () -> Unit
                 }
             }
     }
+}
+
+// Validando as credenciais do usuário
+fun signInWithFirebaseAuth(email: String, password: String, context: Context) {
+    // Obtemos a instância do Firebase Auth
+    val auth = Firebase.auth
+
+    // Tantando realizar o login
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+
+            // Verifica se o login foi bem-sucedido
+            if (task.isSuccessful) {
+                // Guardo o usário autenticado
+                val user = task.result.user
+
+                Log.i("AUTH", "Login realizado com sucesso. UID: ${user?.uid}")
+
+                val intent = Intent(context, MainActivity::class.java)
+                context.startActivity(intent)
+            } else {
+
+                Log.i("AUTH", "Falha ao fazer login.", task.exception)
+
+                // Exibe uma mensagem Toast
+                // Baseado na documentação oficial do Android: https://developer.android.com/guide/topics/ui/notifiers/toasts?hl=pt-br
+                Toast.makeText(context, "Email ou senha incorreta!", Toast.LENGTH_LONG).show()
+            }
+        }
 }
 
